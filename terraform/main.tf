0fb6ebf9796c372d5cd16d81545a69b24f26161f
Akila Pradeep <akilaag@gmail.com>
2026-03-07 12:30:39 +0530

    chore: Retry deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Security Group
 resource "aws_security_group" "app_sg" {
 
-  name = "react-app-sg-terraform"
+  name        = "react-app-sg-terraform"
+  description = "Allow SSH and React App Port"
 
   ingress {
+    description = "SSH Access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -10,6 +17,7 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
+    description = "React App Port"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
@@ -24,13 +32,19 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "app_server" {
 
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   key_name      = "mum-key"
 
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  # VERY IMPORTANT for SSH access
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id
+  ]
 
   user_data = <<-EOF
               #!/bin/bash
